83f979bae63cec08f9ea5244d12a955d31f4098a
Samantha Cedeno <samanthacedenog@gmail.com>
2024-01-10 18:51:32 -0300

    third

--- js/index.js
+++ js/index.js
@@ -1,75 +1,59 @@
-let namestart = prompt("Write your name to start game");
-let agestart = prompt("Write your age");
-let colorstart = prompt("What's your favorite color?");
-let myColor = "purple";
+import Swal from 'sweetalert2';
+window.Swal = Swal;
 
+//Intructions pop up
+let instructionsBtn = document.querySelector ('#instructionsBtn');
 
-//Initial reference for User input settings
-class User {
-    name;
-    age;
-    color;
-
-    constructor (name, age, color) {
-        this.name = name;
-        this.age = age;
-        this.color = color;
-    }
+instructionsBtn.onclick = () => {
+    Swal.fire ({
+        text: "Move the player using the WASD keys. In the meantime move to reach the coin... (Still unable to win until I learn how to code collisions c:)",
+        confirmButtonText: "Accept",
+        confirmButtonColor: "#a7c957",
+    })
 }
 
-const firstUser = new User (namestart, agestart, colorstart );
+//Name input pop up at game start and timer start 
+let startBtn = document.querySelector('#startBtn');
+let playerName = document.querySelector('.playerName');
+let remainingTime = document.querySelector("#remainingTime");
+let secondsLeft = 10;
 
-console.log(`Welcome ${firstUser.name}!! Wow, so you're ${firstUser.age}? That's a great age to be!`);
-let answer = prompt("Now, can you guess my favorite color? You only get one chance!").toLocaleLowerCase();
-
-if (answer === myColor){
-    console.log(`Correct!! But don't worry, I also think ${firstUser.color} is a beautiful color. Now just wait 10 sec to start the game.`);
-} else {
-    console.log(`Wrong answer. You think, ${firstUser.color} is better than any other color? Now just wait 10 sec to start the game`)
-}
-
-
-//Early timer settings for game
-let timer = 0
-
-while (timer <= 10) {
-    loop(timer)
-    timer++;
-}
-
-function loop (timer){
-    setTimeout(function(){
-        console.log(timer);
-    }, 1000 * timer )
-}
-
-
-//Function to start after timer ends.
-function message () {
-    const question = prompt(`Wait a second ${firstUser.name}!! Before starting the game, help me with one thing... what day is it today?`).toLocaleLowerCase();
-    const day = new Date().toLocaleDateString("en-EN", {weekday: "long"}).toLocaleLowerCase();
-
-    if (question == day) {  
-        console.log(`You're right! Today is ${day}, I don't know why some other people tried to give me the full date when all i asked was the day lol. Anyways... Press the wasd keys to move the beautiful rectangle`) }
-        else {
-            console.log(`Mmm... are you sure that's the correct answer to my question?... Today is ${day}... anyways, now try moving the wasd keys to see something never seen before.`)
+startBtn.onclick = () => {
+    Swal.fire ({
+        text: "Please write your name",
+        confirmButtonText: "Start",
+        confirmButtonColor: "#a7c957",
+        input: "text"
+    }).then (answer => {
+        if (answer.isConfirmed) {
+            Swal.fire ({
+                text: "Welcome " + answer.value + ". You can start moving now!",
+                confirmButtonColor: "#a7c957"
+            }).then (answer2 => {
+                if (answer2.isConfirmed) {
+                    localStorage.setItem("playerName", JSON.stringify(answer));
+                    localStorage.getItem('playerName');
+                    
+                    const downloadTimer = setInterval( () => {
+                            if (secondsLeft <= 0) clearInterval(downloadTimer)
+                            remainingTime.value = secondsLeft
+                            remainingTime.textContent = secondsLeft
+                            secondsLeft -= 1
+                        }, 1000)
+                }
+            })
         }
-
-    
+})
 }
-setTimeout(message,10000);
 
+//Player controls
+let player = document.querySelector(".player");
+let coins = document.querySelector(".coins");
 
-//Initial statemens for rectangle/player
-let player = document.querySelector("#player");
-let move = 3;
-let x = 100;
-let y = 250;
-let windowWidth = window.innerWidth;
-let windowHeight = window.innerHeight;
+let move = 35;
+let x = -10;
+let y = -28;
 
-
-//Key bindings for recangle/player.
 let rightPressed = false;
 let leftPressed = false;
 let upPressed = false;
@@ -77,7 +61,7 @@ let downPressed = false;
 let lastKey = '';
 
 //Pressing keys actions
-window.addEventListener ("keydown", (event) => {
+startBtn.addEventListener ("keydown", (event) => {
     switch (event.key) {
         case "d":
             if (event.key === "d") {
@@ -89,7 +73,7 @@ window.addEventListener ("keydown", (event) => {
         break;
         case "a": 
             if (event.key === "a") {
-                lefftPressed = true;
+                leftPressed = true;
                 lastKey = "a"
                 x -= move;
                 player.style.left = x +"px";
@@ -110,41 +94,31 @@ window.addEventListener ("keydown", (event) => {
                 y -= move;
                 player.style.top = y +"px";
             }
-        break;
-        default:
-            console.log("invalid input");
     }
 })
 
 //Releasing keys actions
-window.addEventListener ("keyup", (event) => {
+startBtn.addEventListener ("keyup", (event) => {
     switch (event.key) {
         case "d":
             if (event.key === "d") {
                 rightPressed = false;
-                console.log ("Moving to right no more")
             }
         break;
         case "a": 
             if (event.key === "a") {
-                lefftPressed = false;
-                console.log ("You stopped going left!")
+                leftPressed = false;
             }
         break;
         case "s":
             if (event.key === "s") {
                 downPressed = false;  
-                console.log ("Wont keep moving down")
             }
         break;
         case "w":
             if (event.key === "w") {
                 upPressed = false;
-                console.log ("This stops your up journey.")
             }
-        break;
-        default:
-            console.log("invalid input");
     }
 })
 
